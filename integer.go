a7ff09abcbc9335e6bf04b28366543801d1e04f3
meifakun <runner.mei@gmail.com>
2020-09-02 19:19:54 +0800

    update check

--- integer.go
+++ integer.go
@@ -379,65 +379,6 @@ func numberCheck(argValue interface{}) (func(value interface{}) (int, error), er
 }
 
 func init() {
-	AddCheckFunc(">", "", CheckFactoryFunc(func(argValue interface{}) (Checker, error) {
-		cmp, err := numberCheck(argValue)
-		if err != nil {
-			return nil, ErrArgumentType(">", "integer", argValue)
-		}
-		return CheckFunc(func(value interface{}) (bool, error) {
-			r, err := cmp(value)
-			if err != nil {
-				return false, ErrActualType(">", "integer", value)
-			}
-			//fmt.Printf("1(%T) %v > (%T) %v   = %v\r\n", argValue, argValue, value, value, r)
-			return r < 0, nil
-		}), nil
-	}))
-
-	AddCheckFunc(">=", "", CheckFactoryFunc(func(argValue interface{}) (Checker, error) {
-		cmp, err := numberCheck(argValue)
-		if err != nil {
-			return nil, ErrArgumentType(">=", "integer", argValue)
-		}
-		return CheckFunc(func(value interface{}) (bool, error) {
-			r, err := cmp(value)
-			if err != nil {
-				return false, ErrActualType(">=", "integer", value)
-			}
-
-			//fmt.Printf("2(%T) %v >= (%T) %v   = %v\r\n", argValue, argValue, value, value, r)
-			return r <= 0, nil
-		}), nil
-	}))
-
-	AddCheckFunc("<", "", CheckFactoryFunc(func(argValue interface{}) (Checker, error) {
-		cmp, err := numberCheck(argValue)
-		if err != nil {
-			return nil, ErrArgumentType("<", "integer", argValue)
-		}
-		return CheckFunc(func(value interface{}) (bool, error) {
-			r, err := cmp(value)
-			if err != nil {
-				return false, ErrActualType("<", "integer", value)
-			}
-			return r > 0, nil
-		}), nil
-	}))
-
-	AddCheckFunc("<=", "", CheckFactoryFunc(func(argValue interface{}) (Checker, error) {
-		cmp, err := numberCheck(argValue)
-		if err != nil {
-			return nil, ErrArgumentType("<=", "integer", argValue)
-		}
-		return CheckFunc(func(value interface{}) (bool, error) {
-			r, err := cmp(value)
-			if err != nil {
-				return false, ErrActualType("<=", "integer", value)
-			}
-			return r >= 0, nil
-		}), nil
-	}))
-
 	AddCheckFunc("in", "integer", CheckFactoryFunc(func(argValue interface{}) (Checker, error) {
 		cmp, err := inCheck(argValue, true)
 		if err != nil {
